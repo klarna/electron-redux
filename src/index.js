2d3a59d1d052b41897e488e89f228f4fdfa1548a
Burkhard Reffeling <burkhard.reffeling@sky.uk>
2016-10-12 09:20:24 +0100

    refactor into alias registry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import forwardToMain from './middleware/forwardToMain';
 import forwardToRenderer from './middleware/forwardToRenderer';
-import triggerAlias, { createAliasedAction } from './middleware/triggerAlias';
+import triggerAlias from './middleware/triggerAlias';
+import createAliasedAction from './helpers/createAliasedAction';
 
 export {
   forwardToMain,
--- src/middleware/triggerAlias.js
+++ src/middleware/triggerAlias.js
@@ -1,34 +1,19 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 import assert from 'assert';
-import { ALIASED } from '../actions';
-
-// TODO: move to alias registry
-const aliases = {};
-
-export function createAliasedAction(name, actionCreator) {
-  // register
-  aliases[name] = actionCreator;
-
-  // factory
-  return (...args) => ({
-    type: ALIASED,
-    payload: args,
-    meta: {
-      trigger: name,
-    },
-  });
-}
+import { ALIASED } from '../actions/alias';
+import aliasRegistry from '../registry/alias';
 
 const triggerAlias = store => next => (action) => {
   // TODO: store.dispatch() instead to not skip any middleware
   if (action.type === ALIASED) {
     assert(action.meta && action.meta.trigger, 'No trigger defined');
-    assert(aliases[action.meta.trigger], `Trigger alias ${action.meta.trigger} not found`);
+    const alias = aliasRegistry.get(action.meta.trigger);
+    assert(alias, `Trigger alias ${action.meta.trigger} not found`);
     const args = action.payload || [];
 
     // trigger alias
-    action = aliases[action.meta.trigger](...args);
+    action = alias(...args);
   }
 
   return next(action);
