1f802716f56d5a72a627bc8f3be43b2dca6f9ba9
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2017-11-27 12:02:19 +0100

    add basic integration testing of counter example

---
+++ packages/electron-redux-e2e/specs/counter.spec.js
@@ -0,0 +1,44 @@
+const { Application } = require('spectron');
+
+describe('Counter', () => {
+  let app;
+
+  beforeEach(async () => {
+    app = new Application({
+      path: './node_modules/.bin/electron',
+      args: ['.'],
+    });
+
+    await app.start();
+  });
+
+  afterEach(async () => {
+    if (app && app.isRunning()) {
+      await app.stop();
+    }
+  });
+
+  it('Increases the count by one on click', async () => {
+    expect(await app.client.getText('#value')).toEqual('0');
+
+    await app.client.click('#increment');
+
+    expect(await app.client.getText('#value')).toEqual('1');
+  });
+
+  it('Decreases the count by one on click', async () => {
+    expect(await app.client.getText('#value')).toEqual('0');
+
+    await app.client.click('#decrement');
+
+    expect(await app.client.getText('#value')).toEqual('-1');
+  });
+
+  it('Increases the count by one on clicking the aliased action', async () => {
+    expect(await app.client.getText('#value')).toEqual('0');
+
+    await app.client.click('#incrementAliased');
+
+    expect(await app.client.getText('#value')).toEqual('1');
+  });
+});
