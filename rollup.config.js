9533de71d0a5a9fc905f93a46840a0580737cc01
Maciej Malkowski <maciej.malkowski@klarna.com>
2020-09-22 15:14:21 +0200

    chore: rollup.config simplify

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import replace from "@rollup/plugin-replace";
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
 const extensions = [".ts"];
-const noDeclarationFiles = { compilerOptions: { declaration: false } };
+
+const basePlugins = [
+  commonjs(),
+  nodeResolve({
+    extensions,
+  }),
+  typescript({ useTsconfigDeclarationDir: true }),
+];
 
 export default [
   // CommonJS
@@ -17,14 +24,9 @@ export default [
     output: { file: "lib/electron-redux.js", format: "cjs", indent: false },
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
-      commonjs(),
-      nodeResolve({
-        extensions,
-      }),
-      typescript({ useTsconfigDeclarationDir: true }),
+      ...basePlugins,
       babel({
         extensions,
-        runtimeHelpers: true,
       }),
     ],
   },
@@ -35,14 +37,9 @@ export default [
     output: { file: "es/electron-redux.js", format: "es", indent: false },
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
-      commonjs(),
-      nodeResolve({
-        extensions,
-      }),
-      typescript({ tsconfigOverride: noDeclarationFiles }),
+      ...basePlugins,
       babel({
         extensions,
-        runtimeHelpers: true,
       }),
     ],
   },
@@ -52,14 +49,10 @@ export default [
     input: "src/index.ts",
     output: { file: "es/electron-redux.mjs", format: "es", indent: false },
     plugins: [
-      commonjs(),
-      nodeResolve({
-        extensions,
-      }),
+      ...basePlugins,
       replace({
         "process.env.NODE_ENV": JSON.stringify("production"),
       }),
-      typescript({ tsconfigOverride: noDeclarationFiles }),
       babel({
         extensions,
         exclude: "node_modules/**",
@@ -85,11 +78,7 @@ export default [
       indent: false,
     },
     plugins: [
-      commonjs(),
-      nodeResolve({
-        extensions,
-      }),
-      typescript({ tsconfigOverride: noDeclarationFiles }),
+      ...basePlugins,
       babel({
         extensions,
         exclude: "node_modules/**",
@@ -110,11 +99,7 @@ export default [
       indent: false,
     },
     plugins: [
-      commonjs(),
-      nodeResolve({
-        extensions,
-      }),
-      typescript({ tsconfigOverride: noDeclarationFiles }),
+      ...basePlugins,
       babel({
         extensions,
         exclude: "node_modules/**",
