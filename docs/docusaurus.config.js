e5391125d304719d35b7a38c797a35dc6b5f37f5
António Almeida <theantonioalmeida@gmail.com>
2020-10-29 01:33:14 +0100

    remove trailing slash

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'electron-redux',
   tagline: 'Use redux in the main and browser processes in Electron.',
   url: 'https://klarna.github.io',
-  baseUrl: '/electron-redux/',
+  baseUrl: '/electron-redux',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
   organizationName: 'klarna', 
