6f31172092bb3946281dd791ece0fe1df04abf89
Miroslav Krsjak <mkrsjak@hotmail.com>
2021-04-27 13:57:08 +0200

    Unhandled error fix causing app crash on load

--- packages/electron-redux/src/middleware/forwardToRenderer.js
+++ packages/electron-redux/src/middleware/forwardToRenderer.js
@@ -1,23 +1,25 @@
-import { webContents } from 'electron';
-import validateAction from '../helpers/validateAction';
+import { webContents } from "electron";
+import validateAction from "../helpers/validateAction";
 
-const forwardToRenderer = () => next => (action) => {
+const forwardToRenderer = () => (next) => (action) => {
   if (!validateAction(action)) return next(action);
-  if (action.meta && action.meta.scope === 'local') return next(action);
+  if (action.meta && action.meta.scope === "local") return next(action);
 
   // change scope to avoid endless-loop
   const rendererAction = {
     ...action,
     meta: {
       ...action.meta,
-      scope: 'local',
+      scope: "local",
     },
   };
-
+  if (!webContents) {
+    return next(action);
+  }
   const allWebContents = webContents.getAllWebContents();
 
   allWebContents.forEach((contents) => {
-    contents.send('redux-action', rendererAction);
+    contents.send("redux-action", rendererAction);
   });
 
   return next(action);
